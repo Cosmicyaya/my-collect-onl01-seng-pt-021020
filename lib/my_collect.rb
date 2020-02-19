2ca890ef92946ad653a5b52c4aa61c1438ad1237
Ryan Hollins <ballbaroi@gmail.com>
2020-02-19 17:22:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+students = ["Rebecca Chambers","Chris Redfield","Albet Wesker"]
 
+
+def my_collect(array)
+  i = 0
+  name_collectiion = []
+  while i < array.length
+  name_collection.push yield
+  (arry[i])
+  i += 1
+end
+name_collectiion
+end
 
